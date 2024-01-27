b88464ab69c2b3619015de8705d3c3b5f0815848
Hope-Barnett <63059791+hopebarnett@users.noreply.github.com>
2024-01-27 12:43:43 -0500

    Create main.py
    Fun with Turtle Party Functions!

---
+++ main.py
@@ -0,0 +1,18 @@
+# Turtle Party Project
+# by Hope Barnett
+# 01.27.2024
+print("Hello World")
+
+import math
+
+print(math.sqrt(25))
+
+for i in range(1, 11):
+  print(i)
+  
+for char in "Hello":
+  print(char)
+  
+for i in range(4):
+  print(i)
+  
